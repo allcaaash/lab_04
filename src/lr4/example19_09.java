f90bf7fa853eeae30816a459271454fd850199ea
all_caaash <une066@gmail.com>
2023-12-12 01:25:17 +0500

    edits for 09

--- src/lr4/example19_09.java
+++ src/lr4/example19_09.java
@@ -7,16 +7,6 @@ public class example19_09 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        String alphabet = "abcdefghigklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-        String numbers = "1234567890";
-        String symbols = "~`!@#$%^&*()_+-=[]{}\\|'\"/?.,<>";
-        char[] alphabetArray = new char[alphabet.length()];
-        alphabetArray = alphabet.toCharArray();
-        char[] numbersArray = new char[numbers.length()];
-        numbersArray = numbers.toCharArray();
-        char[] symbolsArray = new char[symbols.length()];
-        symbolsArray = symbols.toCharArray();
-
         System.out.println("Enter the message you want to encrypt:");
         String message = in.nextLine();
         char[] messageArray = new char[message.length()];
@@ -27,67 +17,14 @@ public class example19_09 {
 
         for(int i = 0; i < messageArray.length; i++){
             char curentChar = messageArray[i];
-
-            if (Character.isWhitespace(curentChar)){
-                continue;
-            }
-            else if (Character.isLetter(curentChar)){
-                char tempChar = curentChar;
-                if (Character.isUpperCase(curentChar)){
-                    curentChar = Character.toLowerCase(curentChar);
-                }
-                for (int j = 0; j < alphabetArray.length; j++){
-                    if (curentChar == alphabetArray[j]){
-                        if (j + key <= alphabetArray.length){
-                            curentChar = alphabetArray[j + key];
-                            break;
-                        }
-                        else{
-                            int diff = j + key - alphabetArray.length;
-                            curentChar = alphabetArray[diff];
-                            break;
-                        }
-
-                    }
-                }
-                if (Character.isUpperCase(tempChar) && Character.isLowerCase(curentChar)){
-                    curentChar = Character.toUpperCase(curentChar);
-                }
-                messageArray[i] = curentChar;
+            if (Character.isLetter(curentChar)){
+                messageArray[i] = encryptMessageLetter(curentChar, key);
             }
             else if (Character.isDigit(curentChar)){
-                for (int j = 0; j < numbersArray.length; j++){
-                    if (curentChar == numbersArray[j]){
-                        if (j + key <= numbersArray.length){
-                            curentChar = numbersArray[j + key];
-                            break;
-                        }
-                        else{
-                            int diff = j + key - numbersArray.length;
-                            curentChar = numbersArray[diff];
-                            break;
-                        }
-
-                    }
-                }
-                messageArray[i] = curentChar;
+                messageArray[i] = encryptMessageDigit(curentChar, key);
             }
             else {
-                for (int j = 0; j < symbolsArray.length; j++){
-                    if (curentChar == symbolsArray[j]){
-                        if (j + key <= symbolsArray.length){
-                            curentChar = symbolsArray[j + key];
-                            break;
-                        }
-                        else{
-                            int diff = j + key - symbolsArray.length;
-                            curentChar = symbolsArray[diff];
-                            break;
-                        }
-
-                    }
-                }
-                messageArray[i] = curentChar;
+                messageArray[i] = encryptMessageSymbol(curentChar, key);
             }
         }
 
@@ -95,5 +32,75 @@ public class example19_09 {
         System.out.print("Encrypted message: " + result);
 
         in.close();
+    }
+
+    public static char encryptMessageLetter(char curentChar, int key) {
+        String alphabet = "abcdefghigklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        char[] alphabetArray = new char[alphabet.length()];
+        alphabetArray = alphabet.toCharArray();
+
+        char tempChar = curentChar;
+        if (Character.isUpperCase(curentChar)) {
+            curentChar = Character.toLowerCase(curentChar);
+        }
+        for (int j = 0; j < alphabetArray.length; j++) {
+            if (curentChar == alphabetArray[j]) {
+                if (j + key < alphabetArray.length) {
+                    curentChar = alphabetArray[j + key];
+                    break;
+                } else {
+                    int diff = j + key - alphabetArray.length;
+                    curentChar = alphabetArray[diff];
+                    break;
+                }
+            }
+        }
+        if (Character.isUpperCase(tempChar) && Character.isLowerCase(curentChar)) {
+            curentChar = Character.toUpperCase(curentChar);
+        }
+
+        return curentChar;
+    }
+    public static char encryptMessageDigit(char curentChar, int key) {
+        String numbers = "1234567890";
+        char[] numbersArray = new char[numbers.length()];
+        numbersArray = numbers.toCharArray();
+
+        for (int j = 0; j < numbersArray.length; j++) {
+            if (curentChar == numbersArray[j]) {
+                if (j + key < numbersArray.length) {
+                    curentChar = numbersArray[j + key];
+                    break;
+                } else {
+                    int diff = j + key - numbersArray.length;
+                    curentChar = numbersArray[diff];
+                    break;
+                }
+
+            }
+        }
+
+        return curentChar;
+    }
+    public static char encryptMessageSymbol(char curentChar, int key) {
+        String symbols = "~`!@#$%^&*()_+-=[]{}\\|'\"/?.,<>";
+        char[] symbolsArray = new char[symbols.length()];
+        symbolsArray = symbols.toCharArray();
+
+        for (int j = 0; j < symbolsArray.length; j++) {
+            if (curentChar == symbolsArray[j]) {
+                if (j + key < symbolsArray.length) {
+                    curentChar = symbolsArray[j + key];
+                    break;
+                } else {
+                    int diff = j + key - symbolsArray.length;
+                    curentChar = symbolsArray[diff];
+                    break;
+                }
+
+            }
+        }
+
+        return curentChar;
     }
 }
